c0aa138f1eac73d4048fd20634eb3d2bbf01a760
fwh-mac-mini <fuwenhao594@163.com>
2024-04-29 15:55:08 +0800

    fix:  采集时间和人群

--- twitter_schedule.py
+++ twitter_schedule.py
@@ -1,25 +1,21 @@
 import schedule
 import time
-from twitter_data_ingestion import main as main_ingestion
-from notion_clean_twitter import main as clean_main
+from twitter_data_handle import main as handle_main
 
 def job():
-    #1. 采集数据
-    main_ingestion()
-    # 1.1 清理数据
-    clean_main()
-    # 2. 数据保存到Notion
-
-    print('[Twitter采集程序结束...]')
-    print('[Twitter采集程序运行时间: %s]' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    print('[Twitter采集程序开始. 运行时间: %s]' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    # 一体化: 采集-保存Notion
+    handle_main()
+    print('[Twitter采集程序结束. 运行时间: %s]' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    
     
 
 
 def schedule_job():
-    schedule.every().day.at("08:00").do(job)
+    # schedule.every().day.at("08:00").do(job)
     # schedule.every(10).seconds.do(job)
     # schedule.every(10).minutes.do(job)
-    # schedule.every(1).hours.do(job)
+    schedule.every(2).hours.do(job)
     # schedule.every(1).seconds.do(job)
 
     while True:
